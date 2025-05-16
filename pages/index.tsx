1015bc46cad6f1d89897c9bc2de8198b47ddb94e
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-16 17:31:57 +0530

    added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+
+import React from "react";
 import Image from "next/image";
+import { TbHomeSearch} from "react-icons/tb"
+import {  FaXTwitter} from "react-icons/fa6";
+import { IoIosSearch } from "react-icons/io";
+import { IoNotifications } from "react-icons/io5";
+import { AiOutlineMessage } from "react-icons/ai";
+import { GiAbstract016 } from "react-icons/gi";
+import { CgProfile } from "react-icons/cg";
+import { CiCircleMore } from "react-icons/ci";
+import { IoIosPeople } from "react-icons/io";
+
 import { Geist, Geist_Mono } from "next/font/google";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -10,106 +23,72 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+interface TwitterSidebarButton {
+  title:string;
+  icon:React.ReactNode;
+}
+const sidebarMenuItems :TwitterSidebarButton [ ]= [
+{
+  title:'Home',
+  icon:<TbHomeSearch />
+},
+{
+  title:'Explore',
+  icon:<IoIosSearch />
+},
+{
+  title:'Notification',
+  icon: <IoNotifications />
+},
+{
+  title:'Messages',
+  icon:<AiOutlineMessage />
+},
+{
+  title:'Grok',
+  icon:<GiAbstract016 />
+},
+{
+  title:'Communities',
+  icon:<IoIosPeople />
+},
+{
+  title:'Premium',
+  icon:<FaXTwitter />
+},
+{
+  title:'Profile',
+  icon:<CgProfile />
+},
+{
+  title:'More',
+  icon:<CiCircleMore />
+}
+]
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <div className="grid grid-cols-12 h-screen w-screen px-52 ">
+        <div className="col-span-3 pt-8 ">
+          <div className="text-4xl h-fit hover:bg-gray-600 rounded-full p-1 cursor-pointer transition-all w-fit" >
+            <FaXTwitter    />
+          </div>
+          <div className="mt-3 text-2xl font-semibold">
+                <ul> {sidebarMenuItems.map((item)=>(
+                    <li className="flex  justify-start items-center gap-4  hover:bg-gray-600  rounded-full px-3 py-2 w-fit cursor-pointer transition-all" key={item.title}>
+                      <span>{item.icon}</span>
+                      <span>{item.title}</span>
+                    </li>
+                ))}
+                  
+                </ul>
+                <button className="bg-sky-200 text-black text-lg p-4 mt-5 rounded-full w-full">Post</button>
+            </div>
+         </div>
+        <div className="col-span-6 border-r-2 border-l-2 border-slate-500"></div>
+        <div className="col-span-3"></div>
+      </div>
     </div>
   );
 }
